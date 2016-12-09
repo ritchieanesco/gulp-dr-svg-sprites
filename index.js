6e4960f6e1382362d9e4a9406d333757f10ad10f
Ritchie Anesco <ritchie.anesco@isobar.com>
2016-12-10 08:30:54 +1100

    - add default settings

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 'use strict';
 const gutil = require('gulp-util');
 const through = require('through2');
+const _ = require('lodash');
 const svg = require('dr-svg-sprites');
 const PLUGIN_NAME = 'gulp-dr-svg-sprites';
 
+var defaults = {
+    spriteElementPath: 'svgs',
+    spritePath: 'images',
+    cssPath: 'styles',
+    cssPrefix: 'tmp',
+    sizes: {
+        sm: 12,
+        md: 16,
+        lg: 20,
+        xl: 24
+      },
+    refSize: 'lg',
+    cssPngPrefix: '.no-svg',
+    layout: 'vertical',
+    unit: 10
+  };
+
 module.exports = opts => {
 
-    opts = opts || {};
+    config = _.merge(_.cloneDeep(defaults), opts || {});
 
     return through.obj((file, enc, cb) => {
+
         if (file.isNull()) {
           cb(null, file);
           return;
@@ -21,7 +40,8 @@ module.exports = opts => {
         }
 
         try {
-          file.contents = new Buffer(svg(opts, () => {
+
+          file.contents = new Buffer(svg(config, () => {
               console.log('SVG GENERATION COMPLETE');
               done();
             }));
